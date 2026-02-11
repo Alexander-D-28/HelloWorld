989323060e68482372e45f559b3b22330b5d94be
Alexander-D-28 <justalexander28@gmail.com>
2026-02-11 15:24:13 -0500

    Revert "Added printing numbers 1 through 4"
    This reverts commit 224780144c9076a54816d425263ab3c36e0e21ce.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 public class Welcome {
   public static void main(String[] args) {
     System.out.println("Hello from GitHub Actions");
-    for(int i = 1; i < 5; i++) {
-    System.out.println(i);
-    }
   }
 }
