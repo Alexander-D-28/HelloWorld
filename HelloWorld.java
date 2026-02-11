224780144c9076a54816d425263ab3c36e0e21ce
Alexander Dobek <justalexander28@gmail.com>
2026-02-11 13:33:55 -0500

    Added printing numbers 1 through 4
    Added a for loop to print numbers from 1 to 4 within the HelloWorld.java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class Welcome {
   public static void main(String[] args) {
     System.out.println("Hello from GitHub Actions");
+    for(int i = 1; i < 5; i++) {
+    System.out.println(i);
+    }
   }
 }
